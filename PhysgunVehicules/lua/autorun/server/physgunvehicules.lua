ee1f9afbb68fb1be84e92662df15a55ac8b8dc06
rlevet <36737524+rlevet@users.noreply.github.com>
2018-09-24 18:36:19 +0200

    physgunvehicules.lua => 1.0.1 BETA

--- PhysgunVehicules/lua/autorun/server/physgunvehicules.lua
+++ PhysgunVehicules/lua/autorun/server/physgunvehicules.lua
@@ -1,12 +1,37 @@
--- Allows admins to physgun vehicules. If you have FPP, remove prop_vehicle_jeep from the blocked entities. This script will do the rest. 
+-- Allows admins to physgun vehicules. If you have FPP, remove prop_vehicle_jeep from the blocked entities. This script will do the rest.
+-- This addon doesn't need DarkRP to work.
+
+--[[------------------------------
+		Config
+------------------------------]]--
+
+local PhysgunVehiculesConfig = {} -- Don't touch
+
+local PhysgunVehiculesConfig_Restriction = 1 -- Who can physgun vehicules   0 = No one  1 = admins and superadmins  2 = superadmins only
+
+
+
+--[[------------------------------
+			Code
+------------------------------]]--
 
 local function PhysgunVehicules(ply, ent)
  
 if ( ent:IsVehicle() or (ent:GetClass():lower() == "prop_vehicle_jeep") ) then
-	if ply:IsAdmin() then
-		return true
-	else
+	if (PhysgunVehiculesConfig_Restriction == 0) then
 		return false
+	elseif (PhysgunVehiculesConfig_Restriction == 2) then
+		if ply:IsSuperAdmin() then
+			return true
+		else
+			return false
+		end
+	else
+		if ply:IsAdmin() then
+			return true
+		else
+			return false
+		end
 	end
 end
 
